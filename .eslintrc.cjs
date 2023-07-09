ef7c0bbeba76ec88c482e5fd8c1cc35d40b9de8c
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-09 23:07:50 +0700

    setup project

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+/* eslint-env node */
+
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  overrides: [{ files: ['**/*.cjs', 'vite.config.js'], env: { node: true } }],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/ban-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react/jsx-no-target-blank': 'warn',
+  },
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Library Management FE
+
+## Getting started
+
+- **Start the application**
+
+  1. ```bash
+     npm i
+     ```
+  2. ```bash
+     npm run dev
+     ```
