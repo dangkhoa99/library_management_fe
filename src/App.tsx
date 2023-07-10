239c19321e0ed024c4d608db52c92c1275e9e7d8
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-10 22:54:24 +0700

    add interface ILogin, implement Login Page, handle Login Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { PublicLayout } from '@/common/layout'
+import Login from '@/pages/Login'
+import NotFound from '@/pages/NotFound'
 import { AuthProvider } from '@/provider/AuthProvider'
 import { lightTheme } from '@/themes'
 import '@/themes/styles/App.css'
@@ -5,7 +8,6 @@ import { CssBaseline, ThemeProvider } from '@mui/material'
 import { SnackbarProvider } from 'notistack'
 import { FC } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import NotFound from './pages/NotFound'
 
 const App: FC<{}> = () => {
   return (
@@ -21,6 +23,10 @@ const App: FC<{}> = () => {
           <AuthProvider>
             <Routes>
               <Route path='*' element={<NotFound />} />
+
+              <Route element={<PublicLayout />}>
+                <Route path='/login' element={<Login />} />
+              </Route>
             </Routes>
           </AuthProvider>
         </div>
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,134 @@
+import { ILoginFormValue } from '@/common/interfaces'
+import { useAuth } from '@/hooks/useAuth'
+import Visibility from '@mui/icons-material/Visibility'
+import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import {
+  Alert,
+  Button,
+  Grid,
+  IconButton,
+  InputAdornment,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material'
+import { FC, useCallback, useState } from 'react'
+
+const Login: FC<{}> = () => {
+  const { login } = useAuth()
+
+  const [isShowPassword, setIsShowPassword] = useState(false)
+  const [formValue, setFormValue] = useState<ILoginFormValue>({
+    username: '',
+    password: '',
+  })
+  const [error, setError] = useState<string | undefined>('')
+
+  const toggleShowPassword = useCallback(() => {
+    setIsShowPassword((prev) => !prev)
+  }, [])
+
+  const onFormChangeValue = useCallback((key: string, value: any) => {
+    setError(undefined)
+    setFormValue((prev) => ({
+      ...prev,
+      [key]: value.target.value,
+    }))
+  }, [])
+
+  const handleLogin = useCallback(() => {
+    if (!formValue.username || !formValue.password) {
+      setError('Please enter username and password')
+      return
+    }
+
+    login(formValue, setError)
+  }, [formValue, login])
+
+  const handlePressEnter = useCallback(
+    (e: any) => {
+      if (e.key !== 'Enter') return
+
+      handleLogin()
+    },
+    [handleLogin],
+  )
+
+  return (
+    <Grid
+      container
+      alignContent='center'
+      gap={4}
+      sx={{ height: '100%', width: '550px', m: '0 auto' }}>
+      <Grid item xs={12}>
+        <Typography variant='h4' fontWeight={900}>
+          Khoa Library
+        </Typography>
+      </Grid>
+
+      {error && (
+        <Grid item xs={12}>
+          <Alert severity='error'>{error}</Alert>
+        </Grid>
+      )}
+
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          required
+          size='medium'
+          value={formValue.username}
+          onChange={(e) => onFormChangeValue('username', e)}
+          label='Username'
+          placeholder='Enter your username'
+          onKeyUp={handlePressEnter}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <TextField
+          fullWidth
+          required
+          size='medium'
+          type={isShowPassword ? 'text' : 'password'}
+          value={formValue.password}
+          onChange={(e) => onFormChangeValue('password', e)}
+          label='Password'
+          placeholder='Enter your password'
+          onKeyUp={handlePressEnter}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position='end'>
+                <Tooltip
+                  title={isShowPassword ? 'Hide password' : 'Show password'}>
+                  <span>
+                    <IconButton onClick={toggleShowPassword} edge='end'>
+                      {isShowPassword ? (
+                        <VisibilityOff fontSize='medium' />
+                      ) : (
+                        <Visibility fontSize='medium' />
+                      )}
+                    </IconButton>
+                  </span>
+                </Tooltip>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button
+          fullWidth
+          variant='contained'
+          size='large'
+          sx={{ fontWeight: 900 }}
+          onClick={handleLogin}>
+          Login
+        </Button>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default Login
