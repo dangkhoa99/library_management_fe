33cc23eef536ceba5b089c9b839fe6eafd98f3d3
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2023-07-15 22:49:25 +0700

    add spacing, palette in renderTheme Mui

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -3,10 +3,11 @@ import { createTheme } from '@mui/material/styles'
 
 const renderTheme = (mode: PaletteMode) => {
   return createTheme({
+    spacing: 4,
     palette: {
       mode: mode,
-      primary: { main: '#f57b32' },
-      secondary: { main: '#0095ff' },
+      primary: { main: '#f57b32', light: '#fbd3bb', dark: '#ab5623' },
+      secondary: { main: '#0095ff', light: '#33aaff', dark: '#0068b2' },
     },
     typography: { fontFamily: 'Roboto, Arial, san-serif' },
     zIndex: { appBar: 1200, drawer: 1100 },
